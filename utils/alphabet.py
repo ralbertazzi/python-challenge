4129a669e11a1d02502be64b2ff1cb9046dfccfc
Riccardo Albertazzi <piano.vergato@gmail.com>
2018-01-31 23:08:43 -0800

    Added solution to problem 3

--- utils/alphabet.py
+++ utils/alphabet.py
@@ -5,3 +5,7 @@ def get_lowercase_alphabet():
     alphabet = [chr(ascii_num) for ascii_num in range(ord('a'), ord('z') + 1)]
     return list_of_chars_to_string(alphabet)
 
+def get_uppercase_alphabet():
+
+    alphabet = [chr(ascii_num) for ascii_num in range(ord('A'), ord('Z') + 1)]
+    return list_of_chars_to_string(alphabet)
